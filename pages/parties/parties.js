371cc0f00befbaa09d1159b5b2306d3061c6e15d
vchivu14 <chivu.vlad@yahoo.com>
2021-12-21 03:34:45 +0100

    link to party

--- pages/parties/parties.js
+++ pages/parties/parties.js
@@ -7,13 +7,14 @@ const getHTML = async () => {
     const html = await response.text();
     content.innerHTML = html;
     fetchParties();
-    addListenerForParties();
+    // addListenerForRows();
 }
 
 const fetchParties = async () => {
     const response = await fetch(`${SERVER_URL}/api/parties`);
-    const parties = await response.json();
-    generateHTML(document.querySelector("tbody"), parties);
+    parties = await response.json();
+    const tableBody = document.querySelector("#parties tbody");
+    generateHTML(tableBody, parties);
 }
 
 const generateHTML = (parentElement, parties) => {
@@ -21,25 +22,35 @@ const generateHTML = (parentElement, parties) => {
     parties.forEach((party, i) => {
         HTML += `
             <tr>
-                <a href="/#/parties/${party.id}" data-navigo>
-                    <th data-rowindex=${i}>${i+1}</th>
-                    <th data-rowindex=${i}>${party.name}</th>
-                </a>
+                <th data-rowindex=${i}>${i+1}</th>
+                <th data-rowindex=${i}>${party.name}</th>
+                <a class="btn btn-outline-primary" href="/#/parties/${party.id}" data-navigo>Go</a>
             </tr>
         `
     });
     parentElement.innerHTML = HTML;
 }
 
-const addListenerForParties = () => {
-    const rows = Array.from(document.querySelectorAll("tbody tr"));
-    rows.forEach(row => {
-        row.addEventListener("click", (e) => {
-            console.log("whatsapp");
-            const id = e.target.getAttribute('data-id');
-            location.replace("#/parties/"+id, data-navigo)
-        })
-    })
-}
+// const addListenerForParties = () => {
+//     const rows = Array.from(document.querySelectorAll("tbody tr"));
+//     rows.forEach(row => {
+//         row.addEventListener("click", (e) => {
+//             console.log("whatsapp");
+//             const id = e.target.getAttribute('data-id');
+//             location.replace("#/parties/"+id, data-navigo)
+//         })
+//     })
+// }
+
+// const addListenerForRows = () => {
+//     const rows = Array.from(document.querySelectorAll("tbody tr"));
+//     rows.forEach(row => {
+//         row.addEventListener("click", (e) => {
+//             const partyIndex = e.target.getAttribute('data-rowindex');
+//             console.log(partyIndex);
+//         })
+//     })
+// }
+
 
 export default () => getHTML();
